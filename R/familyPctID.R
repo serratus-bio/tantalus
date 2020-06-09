ba1a4e7e5d3ac0b65bab4441773eae4f0dfd8c12
fransilvion <g.e.novakovsky@gmail.com>
2020-06-09 15:55:14 -0700

    Updated functions

--- R/familyPctID.R
+++ R/familyPctID.R
@@ -6,6 +6,7 @@
 #' @param  score_threshold don't consider family hits with score below this value (0 by default)
 #' @param  x1 left limit on x axis
 #' @param  x2 right limit on x axis
+#' @param log log10 y axis or not (default F)
 #'
 #' @return ggplot
 #'
@@ -15,15 +16,25 @@
 #' 
 #' @export
 #'
-familyPctID <- function(serratus, family_name, score_threshold = 0, title="", x1=0, x2=100){
+familyPctID <- function(serratus, family_name, score_threshold = 0, title="", x1=0, x2=100, log=F){
   family_df <- serratus@family %>% filter(family %in% c(family_name))
   
-  p <- ggplot(data=family_df[which(family_df$score >= score_threshold), ], aes(pctid)) + 
-    geom_histogram(fill="blue", bins = 30) +
-    theme_bw() + 
-    coord_cartesian(xlim=c(x1,x2)) +
-    labs(x = "Percentage Identity") +
-    labs(title = title)
+  if (log==F){
+    p <- ggplot(data=family_df[which(family_df$score >= score_threshold), ], aes(pctid)) + 
+      geom_histogram(fill="blue", bins = 30) +
+      theme_bw() + 
+      coord_cartesian(xlim=c(x1,x2)) +
+      labs(x = "Percentage Identity") +
+      labs(title = title)
+  } else {
+    p <- ggplot(data=family_df[which(family_df$score >= score_threshold), ], aes(pctid)) + 
+      geom_histogram(fill="blue", bins = 30) +
+      theme_bw() + 
+      coord_cartesian(xlim=c(x1,x2)) +
+      scale_y_log10() +
+      labs(x = "Percentage Identity") +
+      labs(title = title)
+  }
   
   return(p)
 }
--- R/familyScore.R
+++ R/familyScore.R
@@ -5,6 +5,7 @@
 #' @param  family_name name of the family
 #' @param  x1 left limit on x axis
 #' @param  x2 right limit on x axis
+#' @param log log10 y axis or not (default F)
 #'
 #' @return ggplot
 #'
@@ -14,15 +15,26 @@
 #' 
 #' @export
 #'
-familyScore <- function(serratus, family_name, x1=0, x2=100){
+familyScore <- function(serratus, family_name, x1=0, x2=100, log=F){
   family_df <- serratus@family %>% filter(family %in% c(family_name))
   
-  p <- ggplot(data=family_df, aes(family_df$score)) + 
-    geom_histogram(fill="blue", bins = 30) +
-    theme_bw() +
-    coord_cartesian(xlim=c(x1,x2)) +
-    labs(x = "Family score") +
-    labs(title = paste(family_name, "family", sep = " "))
+  if (log == F){
+    p <- ggplot(data=family_df, aes(family_df$score)) + 
+      geom_histogram(fill="blue", bins = 30) +
+      theme_bw() +
+      coord_cartesian(xlim=c(x1,x2)) +
+      labs(x = "Family score") +
+      labs(title = paste(family_name, "family", sep = " "))
+  } else {
+    p <- ggplot(data=family_df, aes(family_df$score)) + 
+      geom_histogram(fill="blue", bins = 30) +
+      theme_bw() +
+      scale_y_log10() +
+      coord_cartesian(xlim=c(x1,x2)) +
+      labs(x = "Family score") +
+      labs(title = paste(family_name, "family (log10 y axis)", sep = " "))
+  }
+  
   
   return(p)
 }
