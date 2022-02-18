c9321f0f984b7f4603eebd1e369f28ded1894fe4
Gherman Novakovsky <g.e.novakovsky@gmail.com>
2022-02-18 12:43:19 -0800

    rearranged the files

---
+++ scripts/getBAM.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# getBAM.sh
+
+SRA=$1
+
+S3="s3://lovelywater"
+
+aws s3 cp $S3/bam/"$SRA".bam ./tmp.bam
+aws s3 cp $S3/summary/"$SRA".summary ./summary/
+
+samtools sort tmp.bam > bam/"$SRA".bam
+samtools index bam/"$SRA".bam
