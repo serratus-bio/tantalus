68600bede29bff41f6ec4f27af36a71aa2fc9e67
Luke Pereira <lukejoepereira@gmail.com>
2023-03-29 12:56:34 -0400

    Update namespace refs from lovelywater to lovelywater2

--- R/getSummary.R
+++ R/getSummary.R
@@ -4,36 +4,36 @@
 #' There is an implicit `aws cli` dependency here
 #' Your system should have `aws s3` properly configured for this function
 #' to work.
-#' 
+#'
 #' @param s3_path a string s3 path holding .summary files
 #' @param local_path local path into which to download the data
 #' @keywords Tantalus, S3, summary
 #' @examples
-#' getSummary( s3_path = 's3://lovelywater/summary/',
+#' getSummary( s3_path = 's3://lovelywater2/summary/',
 #'             local_path = '200528_viro')
 #' @export
-#' 
-# s3_path    <- 's3://lovelywater/summary/'
+#'
+# s3_path    <- 's3://lovelywater2/summary/'
 # local_path <- '200528_viro'
 #
 getSummary <- function( s3_path, local_path ){
-  
+
   # TODO: Test that system `aws s3 cp` works
   #       by downloading the token file.
   #       s3://serratus-public/var/aws-test-token.jpg
   # or
   #       Fallback to wget/curl to retrieve files
-  
+
   # TODO: Add test to ensure that the s3_path ends in
   #       <s3_path>/summary/ , else append summary
   #       to avoid people downloading the entire
   #       directory which includes .bam files
- 
+
   systemCMD <- paste0("aws s3 sync ",
                       s3_path,
                       '/summary/ ',
                       local_path,"/")
-  
+
   # TODO: add warning/message suppression?s
   system( systemCMD )
 }
--- R/getSummaryFiles.R
+++ R/getSummaryFiles.R
@@ -1,31 +1,31 @@
 #' getSummaryFiles
 #' Downloads a specified list of summary files and parses them
 #' into a Serratus S4 object
-#' 
+#'
 #' Implicit system dependcies
 #' - aws cli
-#' 
+#'
 #' @param sras a string vector of the SRAs to download
 #' @param s3_path a string s3 path to summary files
-#' @param local_path local path into which to download the data 
+#' @param local_path local path into which to download the data
 #' @keywords Tantalus, S4, Serratus, sra
-#' 
+#'
 #' @return Serratus S4 class
-#' 
+#'
 #' @examples
 #' sras <- c("SRR9701190", "SRR9705127", "SRR975462", "SRR9843091", "SRR9843092")
-#' serr <- getSummaryFiles(sras, 's3://lovelywater/summary/', "summary_files")
-#' 
+#' serr <- getSummaryFiles(sras, 's3://lovelywater2/summary/', "summary_files")
+#'
 #' @export
-#' 
+#'
 getSummaryFiles <- function(sras,
-                            s3_path = 's3://lovelywater/summary/',
+                            s3_path = 's3://lovelywater2/summary/',
                             local_path = 'summary_files'){
-  
+
   summary_files <- paste(sras, "summary", sep=".")
-  
+
   system(paste0( 'mkdir -p ', local_path))
-  
+
   test <- sapply(summary_files, function(x){
     system_getSummary <- paste0("aws s3 cp ",
                                 s3_path, x, " ",
@@ -33,12 +33,12 @@ getSummaryFiles <- function(sras,
     print(system_getSummary)
     system(system_getSummary)
   })
-  
+
   sumWheres <- paste( local_path,
                       system( paste0("ls ", local_path, "*.summary"), intern = T ), sep="/")
-  
+
   serratusObj <- readSerratus( sumWheres )
-  
+
   return(serratusObj)
-  
+
 }
